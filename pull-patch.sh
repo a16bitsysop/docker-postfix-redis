cbdd1026e2c6d2798fe6be6229918be722e7744f
Duncan Bellamy <dunk@denkimushi.com>
2020-10-04 19:47:20 +0100

    try local copy of aport

--- pull-patch.sh
+++ pull-patch.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 # called with aport branch/name eg: main/zsh
 
-if [ -n "$1" ]
-then
-  tobuild="$1"
-else
-  >&2 echo "No aport specified, eg: $0 main/zsh"
-  exit 2
-fi
+#if [ -n "$1" ]
+#then
+#  tobuild="$1"
+#else
+#  >&2 echo "No aport specified, eg: $0 main/zsh"
+#  exit 2
+#fi
 
-echo "Downloading aport files list..."
-afiles=$(wget -qO- "https://git.alpinelinux.org/aports/tree/$tobuild" | \
-grep 'ls-blob' | sed "s+blame+plain+" | sed -r "s+.*ls-blob.*href='(.*)'.*+\1+" | xargs)
+#echo "Downloading aport files list..."
+#afiles=$(wget -qO- "https://git.alpinelinux.org/aports/tree/$tobuild" | \
+#grep 'ls-blob' | sed "s+blame+plain+" | sed -r "s+.*ls-blob.*href='(.*)'.*+\1+" | xargs)
 echo "Extracted filenames: $afiles"
-
-mkdir -p aport
-cd aport
-for afile in $afiles
-do
-  echo "Downloading $afile"
-  wget -q "https://git.alpinelinux.org$afile"
-done
+#
+#mkdir -p aport
+cd postfix
+#for afile in $afiles
+#do
+#  echo "Downloading $afile"
+#  wget -q "https://git.alpinelinux.org$afile"
+#done
 
 echo "Preparing to build $tobuild"
 [ -f ../APKBUILD.patch ] && patch -p1 -i ../APKBUILD.patch
