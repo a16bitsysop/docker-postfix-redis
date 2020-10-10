59fb13cd102ac3d187a8086fa39a4010faa4b209
Duncan Bellamy <dunk@denkimushi.com>
2020-10-10 09:03:22 +0100

    fix lint errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #display environment variables passed with --env
-echo '$REDIS=' $REDIS
-echo '$HOSTNAME=' $HOSTNAME
-echo '$DOMAIN=' $DOMAIN
-echo '$LETSENCRYPT=' $LETSENCRYPT
-echo '$RSPAMD=' $RSPAMD
-echo '$DOVECOT=' $DOVECOT
+echo "\$REDIS= $REDIS"
+echo "\$HOSTNAME= $HOSTNAME"
+echo "\$DOMAIN= $DOMAIN"
+echo "\$LETSENCRYPT= $LETSENCRYPT"
+echo "\$RSPAMD= $RSPAMD"
+echo "\$DOVECOT= $DOVECOT"
 NME=postfix-redis
 set-timezone.sh "$NME"
 
 echo "Configuring from environment variables"
 
 if [ -n "$REDIS" ]; then
-  REDISIP=$(ping -c1 $REDIS | head -n1 | cut -f2 -d'(' | cut -f1 -d')')
-  find /etc/postfix/redis-*.cf -maxdepth 0 -type f -exec sed -e "s+host =.*+host = "$REDISIP"+g" -i '{}' \;
+  REDISIP=$(ping -c1 "$REDIS" | head -n1 | cut -f2 -d'(' | cut -f1 -d')')
+  find /etc/postfix/redis-*.cf -maxdepth 0 -type f -exec sed -e "s+host =.*+host = $REDISIP+g" -i '{}' \;
 fi
 
 if [ -n "$DOMAIN" ]; then
@@ -29,13 +29,13 @@ if [ -n "$HOSTNAME" ]; then
 fi
 
 if [ -n "$RSPAMD" ]; then
-  RSPAMDIP=$(ping -c1 $RSPAMD | head -n1 | cut -f2 -d'(' | cut -f1 -d')')
+  RSPAMDIP=$(ping -c1 "$RSPAMD" | head -n1 | cut -f2 -d'(' | cut -f1 -d')')
   postconf -e "smtpd_milters = inet:$RSPAMDIP:11332"
   postconf -e "non_smtpd_milters = inet:$RSPAMDIP:11332"
 fi
 
 if [ -n "$DOVECOT" ]; then
-  DOVEIP=$(ping -c1 $DOVECOT | head -n1 | cut -f2 -d'(' | cut -f1 -d')')
+  DOVEIP=$(ping -c1 "$DOVECOT" | head -n1 | cut -f2 -d'(' | cut -f1 -d')')
   postconf -e "virtual_transport = lmtp:inet:$DOVEIP"
   sed -i "s+.*smtpd_sasl_path.*+  -o smtpd_sasl_path=inet:$DOVEIP:11330+g" /etc/postfix/master.cf
 fi
